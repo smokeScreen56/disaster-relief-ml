346f0fd07623f0f1a764fa220da304cf7c894a2a
Neel Nemade <neelnemade12345@gmail.com>
2026-02-04 13:44:29 +0530

    Trained Baseline and improved ML model

---
+++ src/decision_engine.py
@@ -0,0 +1,21 @@
+def recommend_action(severity_level):
+    """
+    Maps predicted severity level to relief actions
+    """
+    if severity_level == "Low":
+        return {
+            "priority": 3,
+            "action": "Monitor situation, local authorities handle response"
+        }
+
+    elif severity_level == "Medium":
+        return {
+            "priority": 2,
+            "action": "Deploy medical teams and emergency supplies"
+        }
+
+    else:  # High severity
+        return {
+            "priority": 1,
+            "action": "National emergency response: NDRF, Army, large-scale evacuation"
+        }
